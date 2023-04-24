8a7dc6501c664341c3ed51e9d3392e8d6660f0b4
guilherme <guilhermg23@gmail.com>
2023-04-23 22:01:29 -0300

    debugging

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ client.on('ready', () => {
 })
 
 client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isChatInputCommand()) return
-
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!')
-    }
+    console.log(interaction)
 })
 
-client.login(TOKEN)
+client.login(TOKEN).then(() => {
+    setInterval(() => {
+        console.log('ping')
+    }, 60000)
+})
